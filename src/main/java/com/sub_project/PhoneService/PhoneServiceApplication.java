9aa4baa527c1df14a6bae4eb4145cd3e37883f8c
thaianhhao223 <thaianhhao322@gmail.com>
2021-12-02 14:00:24 +0700

    Hào hoàn thành Phone_Service_Eureka_Client

--- src/main/java/com/sub_project/PhoneService/PhoneServiceApplication.java
+++ src/main/java/com/sub_project/PhoneService/PhoneServiceApplication.java
@@ -2,10 +2,12 @@ package com.sub_project.PhoneService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PhoneServiceApplication {
 
 	public static void main(String[] args) {
